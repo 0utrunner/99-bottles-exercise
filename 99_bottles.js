ae4858d9a4146bbdf67d38af02165c247cc02b44
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-05-23 15:59:33 -0500

    First attempt

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,14 @@
-function bottleSong() {
-  // Write your code here!
-};
-
-bottleSong();
+function bottleSong(num) {
+  let bottles = num;
+  while(bottles > 1){
+    if(bottles == 2){
+      console.log(bottles + ' bottles of beer on the wall, ' + bottles + ' bottles of beer. Take one down and pass it around, ' + (bottles - 1) + ' bottle of beer on the wall.')
+    } else {
+    console.log(bottles + ' bottles of beer on the wall, ' + bottles + ' bottles of beer. Take one down and pass it around, ' + (bottles - 1) + ' bottles of beer on the wall.')
+    }
+    bottles--
+  }
+  return '1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, ' + num + ' bottles of beer on the wall.';
+ };
+ 
+ console.log(bottleSong(10));
