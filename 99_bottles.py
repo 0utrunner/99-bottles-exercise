df09c10ccd8f8b645f3f30fc6f06712b1ba6f82c
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-05-25 14:32:19 -0500

    Python Solved

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,17 @@
-def bottle_song():
-	# write your code here!
-
-
-bottle_song()
+def bottle_song(num):
+	bottles = num
+	while bottles > 1:
+		removed = bottles - 1
+		verse_1 = f'{bottles} bottles of beer on the wall! {bottles} bottles of beer!'
+		verse_2 = f' Take one down and pass it around! {removed} bottles of beer on the wall!'
+		verse_final = f'{bottles} bottles of beer on the wall! {bottles} bottles of beer! Take one down and pass it around! {removed} bottle of beer on the wall!'
+		if bottles == 2:
+			print(verse_final)
+			bottles = bottles - 1
+		else:
+			print(verse_1 + verse_2)
+			bottles = bottles - 1
+		
+	
+	print(f'1 bottle of beer on the wall, 1 bottle of beer! Take one down and pass it around, no more bottles of beer on the wall! No more bottles of beer on the wall, no more bottles of beer! Go to the store and buy some more, {num} bottles of beer on the wall!!')
+bottle_song(5)
